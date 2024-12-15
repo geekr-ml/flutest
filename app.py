865933b84d50d8eb84f8bb228fba60d38710c19e
geekr-ml <demowindowswebhost@gmail.com>
2024-12-15 13:31:59 +0530

    Third Commit

--- app.py
+++ app.py
@@ -77,3 +77,6 @@ def predict():
     except Exception as e:
         print("Error:", str(e))
         return f"An error occurred: {str(e)}", 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
