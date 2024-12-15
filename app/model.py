52f2110434ac7e14c4bb15308b482c0a325762b4
geekr-ml <demowindowswebhost@gmail.com>
2024-12-15 11:13:34 +0530

    Initial Commit

---
+++ app/model.py
@@ -0,0 +1,12 @@
+import pickle
+import numpy as np
+
+def load_model():
+    with open("best_extra_trees_model.pkl", "rb") as f:
+        model = pickle.load(f)
+    return model
+
+def make_prediction(model, features):
+    features = np.array([features])
+    prediction = model.predict(features)[0]
+    return prediction
